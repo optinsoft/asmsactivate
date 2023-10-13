4ff837c078e8b54d1debf5165051e428367b0bd2
vitaly <optinsoft@gmail.com>
2023-10-13 13:56:43 +0500

    v1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='asmsactivate',
-    version='1.0',
+    version='1.1',
     description='Async API wrapper for sms-activate',
     install_requires=["aiohttp","certifi"],
     author='optinsoft',
