a828a83475fa7ce0a1e5fd98f883d28ebfe03b6e
vitaly <optinsoft@gmail.com>
2023-11-23 15:27:23 +0500

    BannedException in __init__

--- asmsactivate/__init__.py
+++ asmsactivate/__init__.py
@@ -1,4 +1,4 @@
 from .asyncsmsactivate import AsyncSmsActivate, AsyncSmsActivateException, \
-    NoSMSException, EarlyCancelException, WrongMaxPriceException
+    NoSMSException, EarlyCancelException, WrongMaxPriceException, BannedException
 from .testsmsactivate import testAsyncSmsActivate
 from .version import __version__
