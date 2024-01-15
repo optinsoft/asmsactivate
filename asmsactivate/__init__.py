9489048d705b1e22ea586b66dc87ca0a2c180cbe
vitaly <optinsoft@gmail.com>
2024-01-15 16:28:31 +0500

    v1.15 __init__ export NoNumbersException

--- asmsactivate/__init__.py
+++ asmsactivate/__init__.py
@@ -1,4 +1,4 @@
 from .asyncsmsactivate import AsyncSmsActivate, AsyncSmsActivateException, \
-    NoSMSException, EarlyCancelException, WrongMaxPriceException, BannedException
+    NoSMSException, EarlyCancelException, NoNumbersException, WrongMaxPriceException, BannedException
 from .testsmsactivate import testAsyncSmsActivate
 from .version import __version__
